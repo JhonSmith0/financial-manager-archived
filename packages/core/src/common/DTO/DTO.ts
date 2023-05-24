f61d846372afb68fd3bbbf8a7aac0015c92e9c05
JhonSmith0 <zamuplays@gmail.com>
2023-05-24 15:51:10 -0300

    ✨ feat: using validation error common on dto

--- packages/core/src/common/DTO/DTO.ts
+++ packages/core/src/common/DTO/DTO.ts
@@ -1,7 +1,15 @@
 import { validate } from "class-validator";
+import ValidationError from "../errors/ValidationError";
 
 export default abstract class DTO {
   public async validate() {
-    return await validate(this);
+    const obj = await validate(this);
+
+    if (!obj.length) return obj;
+
+    return new ValidationError(
+      "Invalid data!",
+      obj.map((e) => e.property)
+    );
   }
 }
