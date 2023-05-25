94643dfaf390661c82fa5e9d543617550dfe218f
JhonSmith0 <zamuplays@gmail.com>
2023-05-24 23:22:21 -0300

    ♻️ refactor: refactor

--- packages/api/src/controllers/AuthController.ts
+++ packages/api/src/controllers/AuthController.ts
@@ -1,49 +1,50 @@
-import { Body, Controller, HttpCode, Post, Res } from '@nestjs/common';
+import { Body, Controller, HttpCode, Post, Res } from "@nestjs/common";
 
-import LoginController from '@financial/core/dist/controllers/auth/LoginController';
-import RegisterController from '@financial/core/dist/controllers/auth/RegisterController';
+import LoginController from "@financial/core/dist/controllers/auth/LoginController";
+import RegisterController from "@financial/core/dist/controllers/auth/RegisterController";
 
-import RegisterUserDTO from '@financial/core/dist/domain/User/dto/CreateUserDTO';
-import LoginUserDTO from '@financial/core/dist/domain/User/dto/LoginUserDTO';
+import RegisterUserDTO from "@financial/core/dist/domain/User/dto/CreateUserDTO";
 
-import ControllerType from '@financial/core/dist/common/Controller';
+import LoginUserDTO from "@financial/core/dist/domain/User/dto/LoginUserDTO";
 
-import { Response } from 'express';
-import adaptErrors from './adapters/adaptErrors';
+import ControllerType from "@financial/core/dist/common/Controller";
 
-@Controller('auth')
+import { Response } from "express";
+import adaptErrors from "./adapters/adaptErrors";
+
+@Controller("auth")
 export default class AuthController {
   constructor(
     private loginController: LoginController,
-    private registerController: RegisterController,
+    private registerController: RegisterController
   ) {}
 
   private async handle<T>(
     controller: ControllerType<T, any>,
     dto: { create(data: T): any },
     res: Response,
-    body: T,
+    body: T
   ) {
     const obj = dto.create(body);
     const result = await controller.handle(obj);
 
-    if (result._tag == 'Left') throw adaptErrors(result.left as any);
+    if (result._tag == "Left") throw adaptErrors(result.left as any);
     this.setJWTOnCookie(res, result.right).end();
   }
 
   private setJWTOnCookie(res: Response, jwt: string) {
-    res.cookie('authorization', jwt);
+    res.cookie("authorization", jwt);
 
     return res;
   }
 
-  @Post('login')
+  @Post("login")
   @HttpCode(204)
   async login(@Body() body: LoginUserDTO, @Res() res: Response) {
     await this.handle(this.loginController, LoginUserDTO, res, body);
   }
 
-  @Post('register')
+  @Post("register")
   @HttpCode(204)
   async register(@Body() body: RegisterUserDTO, @Res() res: Response) {
     await this.handle(this.registerController, RegisterUserDTO, res, body);
