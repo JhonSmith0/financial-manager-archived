fff9b3c9c77215fbcf681e0c7ee7e7d43b16a945
JhonSmith0 <zamuplays@gmail.com>
2023-05-28 14:49:52 -0300

    ✨ feat: transformer

---
+++ packages/core/src/common/Transformer/index.ts
@@ -0,0 +1,19 @@
+import {
+  ClassConstructor,
+  ClassTransformOptions,
+  plainToInstance,
+} from "class-transformer";
+
+export class Transformer {
+  public static toInstanceConfigs: ClassTransformOptions = {
+    excludeExtraneousValues: true,
+  };
+
+  public static plainToInstance<K, T>(
+    cl: ClassConstructor<T>,
+    data: ClassProperties<T> | K,
+    configs?: ClassTransformOptions
+  ) {
+    return plainToInstance(cl, data, { ...this.toInstanceConfigs, ...configs });
+  }
+}
