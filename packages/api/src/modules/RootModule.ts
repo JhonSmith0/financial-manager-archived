270f45682fc3dfb8dea5a62bb63cb0177c0b59fd
JhonSmith0 <zamuplays@gmail.com>
2023-05-30 08:49:22 -0300

    ♻️ refactor: changed repository to prisma

--- packages/api/src/modules/RootModule.ts
+++ packages/api/src/modules/RootModule.ts
@@ -3,16 +3,16 @@ import { Global, Module, Provider } from "@nestjs/common";
 import { ServeStaticModule } from "@nestjs/serve-static";
 import { dirname } from "path";
 
-import AuthModule from "./AuthModule";
-import { RouterModule } from "@nestjs/core";
-import { OnlyLoggedUsers } from "./OnlyLoggedUsers";
 import JWT from "@financial/core/dist/common/JWT/JWT";
 import LoginController from "@financial/core/dist/controllers/auth/LoginController";
 import MeController from "@financial/core/dist/controllers/auth/MeController";
 import RegisterController from "@financial/core/dist/controllers/auth/RegisterController";
 import UserToken from "@financial/core/dist/domain/User/di/UserTokens";
-import UserRepositoryInMemory from "@financial/core/dist/domain/User/repo/UserRepositoryInMemory";
+import UserRepositoryPrisma from "@financial/core/dist/domain/User/repo/UserRepositoryPrisma";
 import CreateUserUseCase from "@financial/core/dist/domain/User/useCases/CreateUserUseCase";
+import { RouterModule } from "@nestjs/core";
+import AuthModule from "./AuthModule";
+import { OnlyLoggedUsers } from "./OnlyLoggedUsers";
 
 const htmlPath = require.resolve("@financial/client/lib/index.html");
 const libPath = dirname(htmlPath);
@@ -20,7 +20,11 @@ const libPath = dirname(htmlPath);
 const providers: Provider[] = [
   {
     provide: UserToken.userRepository,
-    useValue: new UserRepositoryInMemory(),
+    async useFactory() {
+      const repo = new UserRepositoryPrisma();
+      await repo.$connect();
+      return repo;
+    },
   },
   {
     provide: JWT,
