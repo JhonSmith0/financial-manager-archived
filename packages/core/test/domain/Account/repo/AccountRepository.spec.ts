61e8d86c6987e4ea98be1e76fc3e239e9dfae981
JhonSmith0 <zamuplays@gmail.com>
2023-05-31 15:52:48 -0300

    🧪 test: testing prisma on accountRepository

--- packages/core/test/domain/Account/repo/AccountRepository.spec.ts
+++ packages/core/test/domain/Account/repo/AccountRepository.spec.ts
@@ -1,4 +1,15 @@
 import AccountRepositoryInMemory from "@/domain/Account/repo/AccountRepositoryInMemory";
 import { accountRepositoryCommon } from "./AccountRepositoryCommon";
+import { AccountRepositoryPrisma } from "@/domain/Account/repo/AccountRepositoryPrisma";
 
-accountRepositoryCommon("InMemory", new AccountRepositoryInMemory());
+const memory = new AccountRepositoryInMemory();
+const prisma = new AccountRepositoryPrisma();
+
+describe("AccountRepository", () => {
+  beforeAll(async () => {
+    await prisma.$connect();
+  });
+
+  accountRepositoryCommon("InMemory", memory);
+  accountRepositoryCommon("Prisma", prisma);
+});
