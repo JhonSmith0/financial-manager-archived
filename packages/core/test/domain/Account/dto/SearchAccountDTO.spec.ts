7a3a1d7346a008a6f11bc2894f7f164fba46e415
JhonSmith0 <zamuplays@gmail.com>
2023-05-31 15:52:07 -0300

    ✨ feat: searchAccountDTO

---
+++ packages/core/test/domain/Account/dto/SearchAccountDTO.spec.ts
@@ -0,0 +1,22 @@
+import { SearchAccountDTO } from "@/domain/Account/dto/SearchAccountDTO";
+
+describe("SearchAccountDTO", () => {
+  const input = {
+    name: "testing",
+    description: "aaaaaaaaaaaaaaaaaaaaaaa",
+  };
+
+  it("should pass and remove extra prop", () => {
+    const dto = SearchAccountDTO.create({ ...input, extra: "hello" } as any);
+
+    expect(dto).toMatchObject(input);
+    expect(dto).not.toHaveProperty("extra");
+  });
+
+  it("should fail due to invalid type", async () => {
+    const dto = SearchAccountDTO.create({ ...input, name: {} } as any);
+    const validation = await dto.validate();
+    expect(validation.length).toBe(1);
+  });
+
+});
