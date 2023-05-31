5f8a01b960173b810d0707ffd0ee386eedb784b9
JhonSmith0 <zamuplays@gmail.com>
2023-05-31 15:53:39 -0300

    ✨ feat: searchAccountUseCase missing pagination implementation

---
+++ packages/core/src/domain/Account/useCases/SearchAccountUseCases.ts
@@ -0,0 +1,34 @@
+import { UseCase } from "@/common/UseCase";
+import { SearchAccountDTO } from "../dto/SearchAccountDTO";
+import AccountRepository from "../types/AccountRepository";
+import User from "@/domain/User/entity/User";
+
+export interface SearchAccountUseCaseProps {
+  dto: SearchAccountDTO;
+  user: { id: User["id"] };
+}
+
+export class SearchAccountUseCase extends UseCase<SearchAccountUseCaseProps> {
+  constructor(private repo: AccountRepository) {
+    super();
+  }
+
+  public async execute({ dto, user }: SearchAccountUseCaseProps) {
+    const result = await this.repo.findByQuery(
+      {
+        name: {
+          startsWith: dto.name,
+        },
+        description: {
+          startsWith: dto.description,
+        },
+        userId: {
+          eq: user.id,
+        },
+      },
+      0,
+      10
+    );
+    return result;
+  }
+}
