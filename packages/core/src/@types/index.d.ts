c83c7832c7edaf77e144fd84acb4d793d2dd02b1
JhonSmith0 <zamuplays@gmail.com>
2023-05-31 13:53:07 -0300

    ♻️ refactor: moved @types to src

---
+++ packages/core/src/@types/index.d.ts
@@ -0,0 +1,31 @@
+declare global {
+  declare type Matching<T, V> = {
+    [K in keyof T as T[K] extends V ? K : never]: T[K];
+  };
+
+  declare type ClassMethods<T> = Matching<T, Function>;
+  declare type ClassProperties<T> = Omit<T, keyof ClassMethods<T>>;
+
+  declare type OptionalProps<T, K extends keyof T> = Omit<T, K> &
+    Partial<Pick<T, K>>;
+
+  interface ProcessEnv {
+    DB_URL_TEST: string;
+    DB_URL_PROD: string;
+    DB_URL_DEV: string;
+    NODE_ENV: "dev" | "prod" | "test";
+  }
+
+  export declare interface Bao {
+    name: string;
+  }
+
+  namespace NodeJS {
+    interface ProcessEnv {
+      NODE_ENV: "dev" | "prod" | "test";
+      DB_URL: string;
+    }
+  }
+}
+
+export {};
