fe10f09175ed6cb15774c3b9ff6080808f8f3562
JhonSmith0 <zamuplays@gmail.com>
2023-06-02 21:09:48 -0300

    ✨ feat(utils): loadEnv

---
+++ packages/core/src/utils/loadEnv.ts
@@ -0,0 +1,20 @@
+import { config, parse } from "dotenv";
+import { resolve } from "path";
+
+function env(relative: string) {
+  return resolve(__dirname, "../../", relative);
+}
+
+const strategy = {
+  dev: env(".env.dev"),
+  prod: env(".env.prod"),
+  test: env(".env.test"),
+};
+
+export function loadEnv(type: keyof typeof strategy, override = true) {
+  console.log({ type }, type in strategy);
+
+  if (!(type in strategy)) throw new Error("Invalid env type!");
+
+  return config({ path: strategy[type] });
+}
