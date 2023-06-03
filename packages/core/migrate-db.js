7cccf30245c0c55ca8312ab06a1af8d8d43703be
JhonSmith0 <zamuplays@gmail.com>
2023-06-02 21:10:08 -0300

    🚧 build: migrate prisma schema to all dbs on .env files

---
+++ packages/core/migrate-db.js
@@ -0,0 +1,82 @@
+// Migrate all DATABASE_URL on each .env* file
+
+const { exec } = require("child_process");
+const { config, parse, configDotenv, decrypt, populate } = require("dotenv");
+const {
+  readFileSync,
+  writeFileSync,
+  existsSync,
+  rmSync,
+  readdirSync,
+} = require("fs");
+const { resolve, basename } = require("path");
+const { promisify } = require("util");
+
+function readDir(dir) {
+  return readdirSync(dir).map((e) => resolve(dir, e));
+}
+
+function readEnv(path) {
+  if (!existsSync(path)) writeFileSync(path, "");
+
+  return {
+    get basename() {
+      return basename(path);
+    },
+
+    path,
+    get content() {
+      return readFileSync(path, "utf-8");
+    },
+
+    get object() {
+      return parse(this.content) || {};
+    },
+
+    remove() {
+      rmSync(path);
+    },
+
+    stringToObject(string) {
+      return parse(string);
+    },
+
+    objectToString(obj) {
+      return Object.entries(obj)
+        .map((e) => e.join("="))
+        .join("\n");
+    },
+
+    configAsEnv() {
+      config({ path });
+    },
+
+    write(data) {
+      writeFileSync(path, data, "utf-8");
+    },
+
+    copy() {
+      return readEnv(path);
+    },
+  };
+}
+
+const rootEnv = readEnv(resolve(__dirname, ".env"));
+
+const envs = readDir(__dirname).filter((e) => basename(e).startsWith(".env"));
+const envsWithDBurl = envs.map(readEnv).filter((e) => e.object["DATABASE_URL"]);
+
+const execAsync = promisify(exec);
+
+void (async function () {
+  for (const each of envsWithDBurl) {
+    rootEnv.write(each.content);
+
+    console.log(
+      await execAsync(
+        "npx prisma migrate dev --skip-generate --name " + each.basename
+      )
+    );
+  }
+  rootEnv.remove();
+})();
