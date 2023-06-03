3d7fdc56d5f0cafc981d09d4da5607d3022ad0a3
JhonSmith0 <zamuplays@gmail.com>
2023-06-03 09:44:08 -0300

    ✨ feat: account/UpdatedAccountDTO

---
+++ packages/core/test/domain/Account/dto/UpdateAccountDTO.spec.ts
@@ -0,0 +1,82 @@
+import UpdateAccountDTO from "@/domain/Account/dto/UpdateAccountDTO";
+
+function removeProp<T>(obj: T, ...props: (keyof T)[]) {
+  const copy = { ...obj };
+
+  for (const key of props) {
+    //@ts-ignore
+    delete copy[key];
+  }
+
+  return copy;
+}
+
+describe("UpdateAccountDTO", () => {
+  const dataForTest: ClassProperties<UpdateAccountDTO> = {
+    name: "Bank account",
+    description: "testing",
+    id: "lkjlfjljlkj",
+  };
+
+  it("should pass", async () => {
+    const dto = UpdateAccountDTO.create(dataForTest);
+    const errors = await dto.validate();
+
+    expect(errors.length).toBe(0);
+  });
+
+  it("should remove extra props", () => {
+    const dto = UpdateAccountDTO.create({
+      ...dataForTest,
+      extra: "hello",
+    } as any);
+    expect(dto).not.toHaveProperty("extra");
+  });
+
+  it("should give error to missing props", async () => {
+    expect(
+      await UpdateAccountDTO.create(removeProp(dataForTest, "id")).validate()
+    ).toHaveLength(1);
+  });
+
+  it("should test the name", async () => {
+    const small = await UpdateAccountDTO.create({
+      ...dataForTest,
+      name: "123",
+    }).validate();
+    const long = await UpdateAccountDTO.create({
+      ...dataForTest,
+      name: "1".repeat(25),
+    }).validate();
+    const notString = await UpdateAccountDTO.create({
+      ...dataForTest,
+      name: {},
+    } as any).validate();
+
+    expect(small).toHaveLength(1);
+    expect(long).toHaveLength(1);
+    expect(notString).toHaveLength(1);
+  });
+  it("should test the description", async () => {
+    const long = await UpdateAccountDTO.create({
+      ...dataForTest,
+      name: "1".repeat(129),
+    }).validate();
+    const notString = await UpdateAccountDTO.create({
+      ...dataForTest,
+      name: {},
+    } as any).validate();
+
+    expect(long).toHaveLength(1);
+    expect(notString).toHaveLength(1);
+  });
+
+  it("should test id", async () => {
+    expect(
+      await UpdateAccountDTO.create({
+        ...dataForTest,
+        id: {},
+      } as any).validate()
+    ).toHaveLength(1);
+  });
+});
