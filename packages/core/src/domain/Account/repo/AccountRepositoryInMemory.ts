6d1867aec315932e240a2cc3555f97c528e9f32f
JhonSmith0 <zamuplays@gmail.com>
2023-06-02 21:39:08 -0300

    ♻️ refactor: changed findBy...() to findByQuery

--- packages/core/src/domain/Account/repo/AccountRepositoryInMemory.ts
+++ packages/core/src/domain/Account/repo/AccountRepositoryInMemory.ts
@@ -1,12 +1,31 @@
-import { Query } from "@/common/Query";
+import { Query, handlerObj } from "@/common/Query";
 import Account from "../entity";
 import AccountProps from "../types/AccountProps";
 import AccountRepository from "../types/AccountRepository";
 
 export default class AccountRepositoryInMemory implements AccountRepository {
-  findByQuery<T extends AccountProps>(query: Query<T>, skip?: number | undefined, limit?: number | undefined): Promise<AccountProps[]> {
-    throw new Error("Method not implemented.");
+  findByQuery<T extends AccountProps>(
+    query: Query<T>,
+    skip?: number | undefined,
+    limit?: 1 | undefined
+  ): Promise<void | AccountProps>;
+  findByQuery<T extends AccountProps>(
+    query: Query<T>,
+    skip?: number | undefined,
+    limit?: number | undefined
+  ): Promise<AccountProps[]>;
+  public async findByQuery<T extends AccountProps>(
+    query: Query<T>,
+    skip: number = 0,
+    limit: number = 1
+  ): Promise<void | AccountProps | AccountProps[]> {
+    if (limit === 1) return this.data.find((obj) => handlerObj(obj, query));
+
+    return this.data
+      .filter((obj) => handlerObj(obj, query))
+      .slice(skip, skip + limit);
   }
+
   private data: Account[] = [];
 
   public async exists(acc: AccountProps): Promise<boolean> {
