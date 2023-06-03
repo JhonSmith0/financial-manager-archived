d34486123a487e9686f889e6f2d6497a18478d02
JhonSmith0 <zamuplays@gmail.com>
2023-06-02 22:13:22 -0300

    🐛 fix: bug when loading sqlite

--- packages/core/src/utils/loadEnv.ts
+++ packages/core/src/utils/loadEnv.ts
@@ -1,8 +1,15 @@
 import { config, parse } from "dotenv";
-import { resolve } from "path";
+import { basename, resolve } from "path";
 
 function env(relative: string) {
   return resolve(__dirname, "../../", relative);
+}
+
+// If its a file it bugs when the repo is used by another package such as api
+function resolveDatabase(url: string) {
+  if (url.startsWith("file:"))
+    return `file:${resolve(__dirname, "../../prisma/db", basename(url))}`;
+  return url;
 }
 
 const strategy = {
@@ -14,5 +21,11 @@ const strategy = {
 export function loadEnv(type: keyof typeof strategy, override = true) {
   if (!(type in strategy)) throw new Error("Invalid env type!");
 
-  return config({ path: strategy[type] });
+  const obj = config({ path: strategy[type], override });
+
+  process.env["DATABASE_URL"] = resolveDatabase(process.env.DATABASE_URL);
+
+  console.log({ obj: process.env.DATABASE_URL });
+
+  return obj;
 }
