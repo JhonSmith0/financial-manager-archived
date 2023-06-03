fb5370591fd59d9b501a2f846bf423441797dfec
JhonSmith0 <zamuplays@gmail.com>
2023-06-03 10:35:34 -0300

    ✨ feat: deleteAccountUseCase

---
+++ packages/core/src/domain/Account/useCases/DeleteAccountUseCase.ts
@@ -0,0 +1,35 @@
+import { UseCase } from "@/common/UseCase";
+import User from "@/domain/User/entity/User";
+import { DeleteAccountDTO } from "../dto/DeleteAccountDTO";
+import AccountRepository from "../types/AccountRepository";
+import { left, right } from "fp-ts/lib/Either";
+import NotFoundError from "@/common/errors/NotFoundError";
+
+interface Prop {
+  user: { id: User["id"] };
+  dto: ClassProperties<DeleteAccountDTO>;
+}
+
+export class DeleteAccountUseCase extends UseCase<Prop> {
+  constructor(private repo: AccountRepository) {
+    super();
+  }
+
+  public async execute({ dto, user }: Prop) {
+    const exists = await this.repo.findByQuery({
+      id: {
+        equals: dto.id,
+      },
+      userId: {
+        equals: user.id,
+      },
+    });
+
+    if (!exists)
+      return left(
+        new NotFoundError(`Account with id ${dto.id} dont exist!`, ["id"])
+      );
+
+    await this.repo.remove(dto.id);
+  }
+}
---
+++ packages/core/test/domain/Account/dto/DeleteAccountDTO.spec.ts
@@ -0,0 +1,13 @@
+import { DeleteAccountDTO } from "@/domain/Account/dto/DeleteAccountDTO";
+
+describe("DeleteAccountDTO", () => {
+  it("should test id prop", async () => {
+    expect(await DeleteAccountDTO.create({} as any).validate()).toHaveLength(1);
+    expect(
+      await DeleteAccountDTO.create({ id: {} } as any).validate()
+    ).toHaveLength(1);
+    expect(
+      await DeleteAccountDTO.create({ id: "78967786" } as any).validate()
+    ).toHaveLength(0);
+  });
+});
