c9f316bfc3e3f61f7a2007c97edf2f62fc396970
JhonSmith0 <zamuplays@gmail.com>
2023-06-04 10:10:37 -0300

    ✨ feat: list of generic styled components

---
+++ packages/client/src/components/styled/index.tsx
@@ -0,0 +1,52 @@
+import styled from "styled-components";
+
+export const StyledContainer = styled.div``;
+
+export const StyledTitle = styled.h2`
+  font-size: 3.2rem !important;
+`;
+
+export const StyledForm = styled.form``;
+
+export const StyledFieldSet = styled.fieldset`
+  margin-block: 1.2rem;
+  border: none;
+  font-size: 1.8rem;
+`;
+
+export const StyledLabel = styled.label`
+  display: block;
+  margin-bottom: 0.8rem;
+`;
+export const StyledInput = styled.input`
+  font-size: 1.4rem;
+  padding: 0.6rem;
+
+  border: solid 1px #999;
+  outline: none;
+
+  &:focus-visible {
+    background: #f1f3f5;
+  }
+`;
+
+export const StyledButton = styled.button`
+  background: #4c6ef5;
+  color: white;
+  padding: 1.4rem 2.8rem;
+  border-radius: 0.3rem;
+`;
+
+export const StyledBar = styled.div`
+  height: 6.4rem;
+  /* border-bottom: solid 1px #222; */
+  padding: 1.2rem;
+  padding-inline: var(--inline-padding);
+
+  display: flex;
+  align-items: center;
+
+  font-size: 1.8rem;
+`;
+
+export const StyledHomeOutLet = styled.div``;
