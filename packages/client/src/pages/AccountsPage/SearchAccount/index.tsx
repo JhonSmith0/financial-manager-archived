3aa75155ed4de92ddfb69a28addbf45ef84cccc4
JhonSmith0 <zamuplays@gmail.com>
2023-06-04 11:58:14 -0300

    ✨ feat: search accounts on start

--- packages/client/src/pages/AccountsPage/SearchAccount/index.tsx
+++ packages/client/src/pages/AccountsPage/SearchAccount/index.tsx
@@ -8,18 +8,23 @@ import {
 } from "@/components/styled";
 import { SearchAccount } from "@/interface";
 import { search } from "@/state/accountsState";
+import { useEffect } from "react";
 import { useForm } from "react-hook-form";
 import styled from "styled-components";
 
 export const StyledSearchAccount = styled(StyledContainer);
 
 export function SearchAccount() {
-  const { register, handleSubmit } = useForm<SearchAccount>({
+  const { register, handleSubmit, getValues } = useForm<SearchAccount>({
     defaultValues: {
       page: 1,
       name: "",
     },
   });
+
+  useEffect(() => {
+    search(getValues());
+  }, []);
 
   return (
     <StyledForm
