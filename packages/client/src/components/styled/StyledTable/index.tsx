fd7aea719b827d88d46f16387d32490b11dbd13e
JhonSmith0 <zamuplays@gmail.com>
2023-06-05 16:13:42 -0300

    ✨ feat: styledTable

---
+++ packages/client/src/components/styled/StyledTable/index.tsx
@@ -0,0 +1,41 @@
+import styled from "styled-components";
+
+const StyledTable = styled.article`
+  background: #f1f3f5;
+
+  --border-color: #999;
+  border: solid 1px var(--border-color);
+
+  th,
+  td {
+    padding: 0.9rem 1.6rem;
+    border-collapse: collapse;
+
+    &:not(:last-child) {
+      border-right: var(--border-color);
+    }
+  }
+`;
+
+export const StyledTableRow = styled.div`
+  display: grid;
+  grid-template-columns: repeat(6, 1fr);
+
+  &:not(:last-child) {
+    border-bottom: solid 1px var(--border-color);
+  }
+`;
+export const StyledTableRowItem = styled.div`
+  padding: 1.2rem;
+
+  text-overflow: ellipsis;
+  overflow: hidden;
+
+  white-space: nowrap;
+
+  &:not(:last-child) {
+    border-right: solid 1px var(--border-color);
+  }
+`;
+
+export default StyledTable;
