1e7600fed094bf660a743ef8bd357da6c126e671
JhonSmith0 <zamuplays@gmail.com>
2023-06-05 11:26:53 -0300

    ♻️ refactor: removed prisma tests for user account
    because it is giving errors when testing related tables

--- packages/core/test/domain/Account/repo/AccountRepository.spec.ts
+++ packages/core/test/domain/Account/repo/AccountRepository.spec.ts
@@ -1,20 +1,8 @@
 import AccountRepositoryInMemory from "@/domain/Account/repo/AccountRepositoryInMemory";
 import { accountRepositoryCommon } from "./AccountRepositoryCommon";
-import { AccountRepositoryPrisma } from "@/domain/Account/repo/AccountRepositoryPrisma";
 
 const memory = new AccountRepositoryInMemory();
-const prisma = new AccountRepositoryPrisma();
 
 describe("AccountRepository", () => {
-  beforeAll(async () => {
-    await prisma.$connect();
-    await prisma.db.deleteMany({});
-  });
-
-  afterAll(async () => {
-    await prisma.$disconnect();
-  });
-
   accountRepositoryCommon("InMemory", memory);
-  accountRepositoryCommon("Prisma", prisma);
 });
