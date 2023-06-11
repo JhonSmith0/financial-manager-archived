4a9a62982c22b86b401b25cc96f440bddc3c4d16
Jhonsmith0 <zamuplays@gmail.com>
2023-06-11 15:55:52 -0300

    ✨ feat: added validation to see if user owns account
    When trying to create a transaction the useCase will verify if the user really owns the account

--- packages/core/src/domain/Transaction/useCases/CreateTransactionUseCase.ts
+++ packages/core/src/domain/Transaction/useCases/CreateTransactionUseCase.ts
@@ -1,21 +1,58 @@
 import { UseCase } from "@/common/UseCase";
+import NotFoundError from "@/common/errors/NotFoundError";
+import AccountRepository from "@/domain/Account/types/AccountRepository";
 import { CreateTransactionDTO } from "@/domain/Transaction/dto/CreateTransactionDTO";
 import { Transaction } from "@/domain/Transaction/entity";
 import { TransactionRepository } from "@/domain/Transaction/types/TransactionRepository";
-import { right } from "fp-ts/lib/Either";
+import User from "@/domain/User/entity/User";
+import { left, right } from "fp-ts/lib/Either";
 
 interface Props {
   dto: ClassProperties<CreateTransactionDTO>;
+  user: { id: User["id"] };
 }
 
 export class CreateTransactionUseCase extends UseCase<Props> {
-  constructor(private repo: TransactionRepository) {
+  constructor(
+    private transactionRepo: TransactionRepository,
+    private accountRepo: AccountRepository
+  ) {
     super();
   }
 
-  public async execute(data: Props) {
-    const acc = Transaction.create(data.dto);
-    await this.repo.add(acc);
+  private async isAccountsOwner(ids: string[], userId: string) {
+    const [acc1, acc2] = await Promise.all(
+      ids.map((each) =>
+        this.accountRepo.findByQuery({
+          id: {
+            equals: each,
+          },
+          userId: {
+            equals: userId,
+          },
+        })
+      )
+    );
+
+    return acc1 && acc2;
+  }
+
+  public async execute({ dto, user }: Props) {
+    const isOwner = await this.isAccountsOwner(
+      [dto.fromAccountId, dto.toAccountId],
+      user.id
+    );
+
+    if (!isOwner)
+      return left(
+        new NotFoundError("Account not found!", [
+          "fromAccountId",
+          "toAccountId",
+        ])
+      );
+
+    const acc = Transaction.create(dto);
+    await this.transactionRepo.add(acc);
     return right(acc);
   }
 }
--- packages/core/test/domain/Transaction/useCases/CreateTransactionUseCase.spec.ts
+++ packages/core/test/domain/Transaction/useCases/CreateTransactionUseCase.spec.ts
@@ -1,19 +1,42 @@
+import NotFoundError from "@/common/errors/NotFoundError";
 import Account from "@/domain/Account/entity";
+import AccountRepositoryInMemory from "@/domain/Account/repo/AccountRepositoryInMemory";
 import { Transaction } from "@/domain/Transaction/entity";
 import { TransactionRepositoryInMemory } from "@/domain/Transaction/repo/TransactionRepositoryInMemory";
 import { CreateTransactionUseCase } from "@/domain/Transaction/useCases/CreateTransactionUseCase";
 
 describe("CreateTransactionUseCase", () => {
   const repo = new TransactionRepositoryInMemory();
-  const useCase = new CreateTransactionUseCase(repo);
+  const accountsRepo = new AccountRepositoryInMemory();
+
+  const user = { id: "1234" };
+
+  const acc1 = Account.create({
+    description: "",
+    name: "Acc1",
+    userId: user.id,
+  });
+  const acc2 = Account.create({
+    description: "",
+    name: "Acc2",
+    userId: user.id,
+  });
+
+  beforeAll(async () => {
+    await accountsRepo.add(acc1);
+    await accountsRepo.add(acc2);
+  });
+
+  const useCase = new CreateTransactionUseCase(repo, accountsRepo);
 
   it("should create a transaction", async () => {
     const result = await useCase.execute({
       dto: {
         amount: 100,
-        fromAccountId: "1",
-        toAccountId: "2",
+        fromAccountId: acc1.id,
+        toAccountId: acc2.id,
       },
+      user,
     });
 
     //@ts-ignore
@@ -23,5 +46,20 @@ describe("CreateTransactionUseCase", () => {
     expect(await repo.findByQuery({ id: { equals: acc.id } })).toBeInstanceOf(
       Transaction
     );
+  }, 10000);
+  it("should fails because user doesnot own accounts", async () => {
+    const result = await useCase.execute({
+      dto: {
+        amount: 100,
+        fromAccountId: acc1.id,
+        toAccountId: acc2.id,
+      },
+      user: { id: "otherId" },
+    });
+
+    //@ts-ignore
+    const error = result.left as any;
+
+    expect(error).toBeInstanceOf(NotFoundError);
   });
 });
