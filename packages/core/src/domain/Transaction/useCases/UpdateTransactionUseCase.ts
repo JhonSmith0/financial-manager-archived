7807e6f76b6c781c747ece1a59dab5dbc870ec67
JhonSmith0 <zamuplays@gmail.com>
2023-06-14 16:49:54 -0300

    feat: UpdateTransactionUseCase

--- packages/core/src/domain/Transaction/useCases/UpdateTransactionUseCase.ts
+++ packages/core/src/domain/Transaction/useCases/UpdateTransactionUseCase.ts
@@ -1,14 +1,28 @@
 import User from "@/domain/User/entity/User";
 import { UpdateTransactionDTO } from "../dto/UpdateTransactionDTO";
 import { TransactionUseCase } from "./TransactionUseCase";
+import Account from "@/domain/Account/entity";
+import { Transaction } from "../entity";
+import { left, right } from "@/common/ErrorHandlingTypes";
+import NotFoundError from "@/common/errors/NotFoundError";
+import { linkProto } from "@/common/utils/linkProto";
 
 interface Props {
   dto: UpdateTransactionDTO;
-  user: { id: User["id"] };
+  transaction: { id: Transaction["id"] };
 }
 
 export class UpdateTransactionUseCase extends TransactionUseCase {
   public async execute(data: Props) {
-    throw new Error("Method not implemented.");
+    const transaction = await this.transactionRepo.findByQuery(
+      { id: { equals: data.transaction.id } },
+      0,
+      1
+    );
+    if (!transaction)
+      return left(new NotFoundError("Transaction not found", ["id"]));
+
+    await this.transactionRepo.update(transaction.id, data.dto);
+    return right(Transaction.create({ ...transaction, ...data.dto }));
   }
 }
