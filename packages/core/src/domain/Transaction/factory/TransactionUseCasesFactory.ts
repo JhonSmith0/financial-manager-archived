48b5f21bc758cb04c246deb647b0e2230179a2e1
JhonSmith0 <zamuplays@gmail.com>
2023-06-14 14:06:30 -0300

    feat: DeleteAllTransactions

--- packages/core/src/domain/Transaction/factory/TransactionUseCasesFactory.ts
+++ packages/core/src/domain/Transaction/factory/TransactionUseCasesFactory.ts
@@ -2,6 +2,7 @@ import AccountRepository from "@/domain/Account/types/AccountRepository";
 import { TransactionRepository } from "../types/TransactionRepository";
 import { CreateTransactionUseCase } from "../useCases/CreateTransactionUseCase";
 import { SearchTransactionUseCase } from "../useCases/SearchTransactionUseCase";
+import { DeleteAllTransactions } from "../useCases/DeleteAllTransactions";
 
 export class TransactionUseCasesFactory {
   constructor(
@@ -11,4 +12,5 @@ export class TransactionUseCasesFactory {
 
   public create = new CreateTransactionUseCase(this.tRepo, this.aRepo);
   public search = new SearchTransactionUseCase(this.tRepo);
+  public removeAll = new DeleteAllTransactions(this.tRepo);
 }
