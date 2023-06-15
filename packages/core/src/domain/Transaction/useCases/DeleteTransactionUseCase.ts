941c1a212321eab0eb47290dd4a340adf0be7bf1
Jhonsmith0 <zamuplays@gmail.com>
2023-06-14 22:02:09 -0300

    fix: missing limit on deletetransactionUseCase

--- packages/core/src/domain/Transaction/useCases/DeleteTransactionUseCase.ts
+++ packages/core/src/domain/Transaction/useCases/DeleteTransactionUseCase.ts
@@ -9,23 +9,22 @@ interface Prop {
 
 export class DeleteTransactionUseCase extends TransactionUseCase {
   public async execute(data: Prop) {
-    const transaction = await this.transactionRepo.findByQuery({
-      id: {
-        equals: data.dto.id,
+    const transaction = await this.transactionRepo.findByQuery(
+      {
+        id: {
+          equals: data.dto.id,
+        },
       },
-    });
+      undefined,
+      1
+    );
 
     if (!transaction)
       return left(new NotFoundError("Account not found!", ["id"]));
 
-    await this.transactionRepo.deleteByQuery(
-      {
-        id: {
-          equals: transaction.id,
-        },
-      },
-      1
-    );
+    await this.transactionRepo.deleteByQuery({
+      id: { equals: transaction.id },
+    } as any);
 
     return right(null);
   }
