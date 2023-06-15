6124db35f5fccf3eaa3aa2d3bcb2b2e8758efaf2
Jhonsmith0 <zamuplays@gmail.com>
2023-06-15 20:48:22 -0300

    feat: now when a new transaction is created it updates the state

--- packages/client/src/controllers/transaction/newTransactionController.ts
+++ packages/client/src/controllers/transaction/newTransactionController.ts
@@ -1,9 +1,11 @@
 import { TransactionCreate } from "@/interface";
 import { newTransactionService } from "@/services/transaction";
 import { addTransaction } from "@/state/transaction/transactionsState";
+import { getTransactionsController } from ".";
 
 export async function newTransactionController(data: TransactionCreate) {
   const result = await newTransactionService(data);
-  addTransaction(result);
+  await getTransactionsController({});
   return result;
 }
+
