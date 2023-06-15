d3ed52556a41d6d14872b12fca2065120aaf5d80
Jhonsmith0 <zamuplays@gmail.com>
2023-06-14 22:04:24 -0300

    feat(client): removeTransactionController

--- packages/client/src/services/transaction/index.ts
+++ packages/client/src/services/transaction/index.ts
@@ -1,9 +1,14 @@
 import { ITransaction, GetTransactions, TransactionCreate } from "@/interface";
 import api from "../http";
 
+export async function removeTransactionService(data: Pick<ITransaction, "id">) {
+  return (await api.delete(`/api/transaction/${data.id}`)).data as void;
+}
+
 export async function newTransactionService(data: TransactionCreate) {
   return (await api.post("/api/transaction", data)).data as ITransaction;
 }
+
 export async function getTransactionService(data: GetTransactions) {
   return (await api.get("/api/transaction/search", { params: data })).data as {
     results: ITransaction[];
