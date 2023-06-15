2d71dbb8b1d141e0e45ac0dd10802956d983b985
Jhonsmith0 <zamuplays@gmail.com>
2023-06-15 20:46:24 -0300

    refactor: new type for transactions state

--- packages/client/src/state/transaction/transactionsState.ts
+++ packages/client/src/state/transaction/transactionsState.ts
@@ -1,11 +1,11 @@
-import { ITransaction } from "@/interface";
+import { ITransactionWithAccounts } from "@/interface";
 import { hookstate } from "@hookstate/core";
 
-const transactionsState = hookstate<ITransaction[]>([]);
+const transactionsState = hookstate<ITransactionWithAccounts[]>([]);
 
-export function addTransaction(transaction: ITransaction) {
+export function addTransaction(transaction: ITransactionWithAccounts) {
   transactionsState.merge([transaction]);
 }
 
-
 export default transactionsState;
+
