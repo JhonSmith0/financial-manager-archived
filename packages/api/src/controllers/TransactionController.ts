07c4dcb62d51a2e4b65c1f9183a9592f2d3680a0
Jhonsmith0 <zamuplays@gmail.com>
2023-06-14 22:03:14 -0300

    feat(transaction): patch, delete controller

--- packages/api/src/controllers/TransactionController.ts
+++ packages/api/src/controllers/TransactionController.ts
@@ -1,13 +1,19 @@
 import { LeftRightHandler } from "@financial/core/dist/common/decorators/LeftRightHandler";
 import { CreateTransactionDTO } from "@financial/core/dist/domain/Transaction/dto/CreateTransactionDTO";
+import { DeleteTransactionDTO } from "@financial/core/dist/domain/Transaction/dto/DeleteTransactionDTO";
+import { ReadTransactionDTO } from "@financial/core/dist/domain/Transaction/dto/ReadTransactionDTO";
 import { SearchTransactionDTO } from "@financial/core/dist/domain/Transaction/dto/SearchTransactionDTO";
+import { UpdateTransactionDTO } from "@financial/core/dist/domain/Transaction/dto/UpdateTransactionDTO";
 import { TransactionUseCasesFactory } from "@financial/core/dist/domain/Transaction/factory/TransactionUseCasesFactory";
 import User from "@financial/core/dist/domain/User/entity/User";
 import {
   Body,
   Controller,
+  Delete,
   Get,
-  Post
+  Param,
+  Patch,
+  Post,
 } from "@nestjs/common";
 import { AdaptErrors } from "../adapters/adaptErrors";
 import { UserEntity } from "../decorators/UserEntity";
@@ -35,5 +41,45 @@ export class TransactionController {
     const validation = await dto.validate();
     if (validation.length) throw validation;
     return await this.transactionUseCases.search.execute({ dto, user });
+  }
+
+  @Get("/:id")
+  @AdaptErrors()
+  @LeftRightHandler()
+  public async readTransaction(@Param() params: any) {
+    const dto = new ReadTransactionDTO(params);
+    const validation = await dto.validate();
+    if (validation.length) throw validation;
+    return await this.transactionUseCases.read.execute(dto.id);
+  }
+
+  @Delete("/:id")
+  @AdaptErrors()
+  @LeftRightHandler()
+  public async removeTransaction(@Param() params: any) {
+    const dto = new DeleteTransactionDTO(params);
+    const validation = await dto.validate();
+    if (validation.length) throw validation;
+    return await this.transactionUseCases.remove.execute({
+      dto: { id: dto.id },
+    });
+  }
+
+  @Patch("/:id")
+  @AdaptErrors()
+  @LeftRightHandler()
+  public async updateTransaction(
+    @Param() params: any,
+    @Body() body: UpdateTransactionDTO
+  ) {
+    const dto = new UpdateTransactionDTO(body);
+    const validation = await dto.validate();
+    if (validation.length) throw validation;
+    return await this.transactionUseCases.update.execute({
+      dto,
+      transaction: {
+        id: params.id,
+      },
+    });
   }
 }
