75c9c85310babdd3e4d7fb2ab10677fbadbbbc01
JhonSmith0 <zamuplays@gmail.com>
2023-06-15 11:10:01 -0300

    refactor: added close and open functions to card

--- packages/client/src/components/TransactionRow.tsx
+++ packages/client/src/components/TransactionRow.tsx
@@ -1,44 +1,61 @@
 import { StyledIconsList, StyledInput } from "@/components/styled";
 import {
-  StyledTableRow,
-  StyledTableRowItem,
+    StyledTableRow,
+    StyledTableRowItem,
 } from "@/components/styled/StyledTable";
 import { useTransactionRow } from "@/hooks/transactions/useTransactionRow";
 import { ITransaction } from "@/interface";
-import { FiTrash2 } from "react-icons/fi";
 import { BiCog } from "react-icons/bi";
-import { removeTransactionController } from "@/controllers/transaction/removeTransactionController";
+import { FiTrash2 } from "react-icons/fi";
+import {
+    StyledUpdateTransaction,
+    UpdateTransaction,
+} from "./UpdateTransaction";
+import styled from "styled-components";
 
 interface Props {
-  data: ITransaction;
+    data: ITransaction;
 }
 
-export function TransactionRow({ data }: Props) {
-  const { fromAccount, toAccount, remove } = useTransactionRow(data);
+export const StyledTransactionRow = styled(StyledTableRow)`
+    position: relative;
+    ${StyledUpdateTransaction} {
+        position: absolute;
+    }
+`;
 
-  return (
-    <StyledTableRow>
-      <StyledTableRowItem>
-        <StyledInput
-          value={new Date(data.date).toISOString().slice(0, 10)}
-          type="date"
-          disabled
-        />
-      </StyledTableRowItem>
-      <StyledTableRowItem>R$ {data.amount}</StyledTableRowItem>
-      <StyledTableRowItem>{data.description}</StyledTableRowItem>
-      <StyledTableRowItem>{fromAccount?.name}</StyledTableRowItem>
-      <StyledTableRowItem>{toAccount?.name}</StyledTableRowItem>
-      <StyledTableRowItem>
-        <StyledIconsList>
-          <button onClick={remove}>
-            <FiTrash2 />
-          </button>
-          <button>
-            <BiCog />
-          </button>
-        </StyledIconsList>
-      </StyledTableRowItem>
-    </StyledTableRow>
-  );
+export function TransactionRow({ data }: Props) {
+    const { isOpen, fromAccount, toAccount,close, open, remove } =
+        useTransactionRow(data);
+
+    
+
+    return (
+        <>
+            {isOpen && <UpdateTransaction transaction={data} onClose={close} />}
+            <StyledTransactionRow>
+                <StyledTableRowItem>
+                    <StyledInput
+                        value={new Date(data.date).toISOString().slice(0, 10)}
+                        type="date"
+                        disabled
+                    />
+                </StyledTableRowItem>
+                <StyledTableRowItem>R$ {data.amount}</StyledTableRowItem>
+                <StyledTableRowItem>{data.description}</StyledTableRowItem>
+                <StyledTableRowItem>{fromAccount?.name}</StyledTableRowItem>
+                <StyledTableRowItem>{toAccount?.name}</StyledTableRowItem>
+                <StyledTableRowItem>
+                    <StyledIconsList>
+                        <button onClick={remove}>
+                            <FiTrash2 />
+                        </button>
+                        <button onClick={open}>
+                            <BiCog />
+                        </button>
+                    </StyledIconsList>
+                </StyledTableRowItem>
+            </StyledTransactionRow>
+        </>
+    );
 }
--- packages/client/src/hooks/transactions/useTransactionRow.ts
+++ packages/client/src/hooks/transactions/useTransactionRow.ts
@@ -1,31 +1,45 @@
 import { readAccountController } from "@/controllers/account";
-import { removeTransactionController } from "@/controllers/transaction/removeTransactionController";
+import { removeTransactionController } from "@/controllers/transaction";
 import { IAccount, ITransaction } from "@/interface";
 import { useHookstate } from "@hookstate/core";
 import { useEffect } from "react";
 
 export function useTransactionRow(data: ITransaction) {
-  const fromAccountState = useHookstate<IAccount | null>(null);
-  const toAccountState = useHookstate<IAccount | null>(null);
+    const fromAccountState = useHookstate<IAccount | null>(null);
+    const toAccountState = useHookstate<IAccount | null>(null);
 
-  const fromAccount = fromAccountState.ornull?.get();
-  const toAccount = toAccountState.ornull?.get();
+    const fromAccount = fromAccountState.ornull?.get();
+    const toAccount = toAccountState.ornull?.get();
 
-  useEffect(() => {
-    (async function () {
-      const from = await readAccountController(data.fromAccountId);
-      const to = await readAccountController(data.toAccountId);
-      from && fromAccountState.set(from);
-      to && toAccountState.set(to);
-    })();
-  }, [data]);
+    useEffect(() => {
+        (async function () {
+            const from = await readAccountController(data.fromAccountId);
+            const to = await readAccountController(data.toAccountId);
+            from && fromAccountState.set(from);
+            to && toAccountState.set(to);
+        })();
+    }, [data]);
 
-  return {
-    fromAccount,
-    toAccount,
-    async remove() {
-      if (!confirm("Are you sure?")) return;
-      await removeTransactionController(data);
-    },
-  };
+    const openState = useHookstate(false);
+    const isOpen = openState.get();
+
+    return {
+        fromAccount,
+        toAccount,
+        isOpen,
+
+        async remove() {
+            if (!confirm("Are you sure!")) return;
+            await removeTransactionController(data);
+        },
+        toggleOpen() {
+            openState.set(!isOpen);
+        },
+        close() {
+            openState.set(false)
+        }, 
+        open() {
+            openState.set(true)
+        }
+    };
 }
