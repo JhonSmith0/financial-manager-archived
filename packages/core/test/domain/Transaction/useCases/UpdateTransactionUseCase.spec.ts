3d3b40cc89b5d0a0bc9411f336669418e7b8d97d
Jhonsmith0 <zamuplays@gmail.com>
2023-06-17 12:37:05 -0300

    refactor: adapted update transaction use case

--- packages/core/test/domain/Transaction/useCases/UpdateTransactionUseCase.spec.ts
+++ packages/core/test/domain/Transaction/useCases/UpdateTransactionUseCase.spec.ts
@@ -1,43 +1,53 @@
 import NotFoundError from "@/common/errors/NotFoundError";
+import { AccountRepository } from "@/domain/Account/repo/AccountRepository";
+import CreateAccountUseCase from "@/domain/Account/useCases/CreateAccountUseCase";
 import { Transaction } from "@/domain/Transaction/entity";
 import { TransactionRepository } from "@/domain/Transaction/repo/TransactionRepository";
-
+import { CreateTransactionUseCase } from "@/domain/Transaction/useCases/CreateTransactionUseCase";
+import UserRepository from "@/domain/User/repo/UserRepository";
+import CreateUserUseCase from "@/domain/User/useCases/CreateUserUseCase";
+import { usersForTests, genAccounts, genTransactions } from "../../../setup";
 import { UpdateTransactionUseCase } from "@/domain/Transaction/useCases/UpdateTransactionUseCase";
 
 describe("UpdateTransactionUseCase", () => {
-	const repo = new TransactionRepositoryInMemory();
-	const useCase = new UpdateTransactionUseCase(repo);
+	const tranRepo = new TransactionRepository();
+	const accRepo = new AccountRepository();
+	const userRepo = new UserRepository();
 
-	const transactions = Array.from({ length: 10 }, (_, i) =>
-		Transaction.create({
-			amount: 100,
-			fromAccountId: "1",
-			toAccountId: "2",
-			userId: "123",
-		})
-	);
+	const user = usersForTests[0];
+	const from = genAccounts(user)[0];
+	const to = genAccounts(user)[1];
+
+	const [transaction] = genTransactions(from, to, user, 1);
+
+	const createTransaction = new CreateTransactionUseCase(tranRepo);
+	const updateTransaction = new UpdateTransactionUseCase(tranRepo);
 
 	beforeAll(async () => {
-		for (const each of transactions) {
-			await repo.add(each);
-		}
-	});
+		const createUser = new CreateUserUseCase(userRepo);
+		const createAccount = new CreateAccountUseCase(accRepo);
 
+		await createUser.execute(user);
+		await createAccount.execute(from);
+		await createAccount.execute(to);
+
+		await createTransaction.execute({ dto: transaction, user });
+	});
 	it("should say account dont exists", async () => {
-		const result = await useCase.execute({
+		const result = await updateTransaction.execute({
 			dto: {} as any,
-			transaction: { id: transactions[0].id + "a" },
+			transaction: { id: "a" },
 		});
 		expect(result.isLeft()).toBeTruthy();
 		expect(result.value).toBeInstanceOf(NotFoundError);
 	});
 
 	it("should update just fine", async () => {
-		const result = await useCase.execute({
+		const result = await updateTransaction.execute({
 			dto: {
 				amount: 200,
 			} as any,
-			transaction: { id: transactions[0].id },
+			transaction: { id: transaction.id },
 		});
 		expect(result.isRight()).toBeTruthy();
 		expect(result.value).toBeInstanceOf(Transaction);
