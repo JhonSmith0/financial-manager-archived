b07c09c6c29e590ec8ad423be775f9abd107d05b
Jhonsmith0 <zamuplays@gmail.com>
2023-06-17 13:04:44 -0300

    refactor: remove repository tests

--- packages/core/test/domain/Account/repo/AccountRepository.spec.ts
+++
@@ -1,9 +0,0 @@
-import AccountRepositoryInMemory from "@/domain/Account/repo/AccountRepositoryInMemory";
-import { accountRepositoryCommon } from "./AccountRepositoryCommon";
-
-const repo = new AccountRepositoryInMemory();
-
-describe("AccountRepository", () => {
-  accountRepositoryCommon("InMemory", repo);
-});
-
