dadbe370d8525bda13e7da4be726fc1e5afda56e
Jhonsmith0 <zamuplays@gmail.com>
2023-06-17 12:33:59 -0300

    refactor: adapted readTransactionUseCase

--- packages/core/test/domain/Transaction/useCases/ReadTransactionUseCase.spec.ts
+++ packages/core/test/domain/Transaction/useCases/ReadTransactionUseCase.spec.ts
@@ -1,50 +1,49 @@
 import NotFoundError from "@/common/errors/NotFoundError";
-import Account from "@/domain/Account/entity";
-import AccountRepositoryInMemory from "@/domain/Account/repo/AccountRepositoryInMemory";
-import { Transaction } from "@/domain/Transaction/entity";
+import { AccountRepository } from "@/domain/Account/repo/AccountRepository";
+import CreateAccountUseCase from "@/domain/Account/useCases/CreateAccountUseCase";
 import { TransactionWithAccounts } from "@/domain/Transaction/entity/TransactionWithAccounts";
 import { TransactionRepository } from "@/domain/Transaction/repo/TransactionRepository";
 
 import { TransactionWithAccountsProps } from "@/domain/Transaction/types/TransactionWithAccountsProps";
+import { CreateTransactionUseCase } from "@/domain/Transaction/useCases/CreateTransactionUseCase";
 import ReadTransactionUseCase from "@/domain/Transaction/useCases/ReadTransactionUseCase";
+import UserRepository from "@/domain/User/repo/UserRepository";
+import CreateUserUseCase from "@/domain/User/useCases/CreateUserUseCase";
+import { usersForTests, genAccounts, genTransactions } from "../../../setup";
 
 describe("ReadTransactionUseCase", () => {
-	const accountRepo = new AccountRepositoryInMemory();
-	const repo = new TransactionRepositoryInMemory(accountRepo);
-	const useCase = new ReadTransactionUseCase(repo);
+	const tranRepo = new TransactionRepository();
+	const accRepo = new AccountRepository();
+	const userRepo = new UserRepository();
 
-	const acc2 = Account.create({
-		description: "",
-		name: "12355",
-		userId: "1234",
-	});
-	const acc1 = Account.create({
-		description: "",
-		name: "123555",
-		userId: "1234",
-	});
+	const user = usersForTests[0];
+	const from = genAccounts(user)[0];
+	const to = genAccounts(user)[1];
 
-	const transaction = Transaction.create({
-		amount: 10,
-		fromAccountId: acc1.id,
-		toAccountId: acc2.id,
-		userId: "123",
-	});
+	const [transaction1] = genTransactions(from, to, user, 1);
+
+	const createTransaction = new CreateTransactionUseCase(tranRepo);
+	const readTransaction = new ReadTransactionUseCase(tranRepo);
 
 	beforeAll(async () => {
-		await accountRepo.add(acc1);
-		await accountRepo.add(acc2);
-		await repo.add(transaction);
+		const createUser = new CreateUserUseCase(userRepo);
+		const createAccount = new CreateAccountUseCase(accRepo);
+
+		await createUser.execute(user);
+		await createAccount.execute(from);
+		await createAccount.execute(to);
+
+		await createTransaction.execute({ dto: transaction1, user });
 	});
 
 	it("should give not found error", async () => {
-		const result = await useCase.execute("invalid id");
+		const result = await readTransaction.execute("invalid id");
 
 		expect(result.isLeft()).toBeTruthy();
 		expect(result.value).toBeInstanceOf(NotFoundError);
 	});
 	it("should give right with the transaction", async () => {
-		const result = await useCase.execute(transaction.id);
+		const result = await readTransaction.execute(transaction1.id);
 
 		expect(result.isRight()).toBeTruthy();
 
