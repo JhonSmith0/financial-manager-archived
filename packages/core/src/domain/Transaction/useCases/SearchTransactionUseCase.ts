d720b8505ecc17925d779be2e01d6f3a11ff5a45
Jhonsmith0 <zamuplays@gmail.com>
2023-06-17 15:53:12 -0300

    feat: searchTransactions with infos

--- packages/core/src/domain/Transaction/useCases/SearchTransactionUseCase.ts
+++ packages/core/src/domain/Transaction/useCases/SearchTransactionUseCase.ts
@@ -23,6 +23,10 @@ export class SearchTransactionUseCase extends UseCase<Props> {
 			where: {
 				userId: data.user.id,
 			},
+			include: {
+				fromAccount: {},
+				toAccount: {},
+			},
 		})) as TransactionProps[];
 
 		return right({
