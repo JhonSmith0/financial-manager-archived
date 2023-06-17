db167d93317b573c9ed9b4f2aa424ca792a8320c
Jhonsmith0 <zamuplays@gmail.com>
2023-06-17 15:53:33 -0300

    feat: setTransactionsState

--- packages/client/src/state/transaction/transactionsState.ts
+++ packages/client/src/state/transaction/transactionsState.ts
@@ -6,6 +6,8 @@ const transactionsState = hookstate<ITransactionWithAccounts[]>([]);
 export function addTransaction(transaction: ITransactionWithAccounts) {
 	transactionsState.merge([transaction]);
 }
+export function setTransactions(transactions: ITransactionWithAccounts[]) {
+	transactionsState.merge(transactions);
+}
 
 export default transactionsState;
-
