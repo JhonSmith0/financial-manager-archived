ae4f6f788053a4ff1723f80861324f7f6c7a9546
Jhonsmith0 <zamuplays@gmail.com>
2023-06-17 14:31:47 -0300

    test: addded swc to jest

--- packages/core/jest.config.ts
+++ packages/core/jest.config.ts
@@ -3,6 +3,21 @@ import { pathsToModuleNameMapper } from "ts-jest";
 // which contains the path mapping (ie the `compilerOptions.paths` option):
 import { compilerOptions } from "./tsconfig.json";
 import type { JestConfigWithTsJest } from "ts-jest";
+import { Config } from "@swc/core";
+
+const config: Config = {
+	jsc: {
+		parser: {
+			syntax: "typescript",
+			dynamicImport: false,
+			decorators: true,
+		},
+		target: "es2022",
+		loose: false,
+		externalHelpers: false,
+		keepClassNames: false,
+	},
+};
 
 const jestConfig: JestConfigWithTsJest = {
 	// [...]
@@ -13,11 +28,11 @@ const jestConfig: JestConfigWithTsJest = {
 		compilerOptions.paths /*, { prefix: '<rootDir>/' } */
 	),
 	// setupFiles: ["./test/setup/index.ts"],
-  
-	// transform: {
-	// 	"^.+\\.(t|j)sx?$": "@swc/jest",
-	// },
-  setupFilesAfterEnv: ['./test/setup/index.ts']
+
+	transform: {
+		"^.+\\.(t|j)sx?$": ["@swc/jest", config as any],
+	},
+	setupFilesAfterEnv: ["./test/setup/index.ts"],
 };
 
 export default jestConfig;
