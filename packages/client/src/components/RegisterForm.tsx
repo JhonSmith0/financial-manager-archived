1a75fe3850dd29b955f3a720dcd5bd5ed46e2f46
Jhonsmith0 <zamuplays@gmail.com>
2023-06-18 17:18:26 -0300

    fix: fixed circular function

--- packages/client/src/components/RegisterForm.tsx
+++ packages/client/src/components/RegisterForm.tsx
@@ -1,63 +1,64 @@
-import useRegisterForm from "@/hooks/auth/useRegisterForm";
-import { IRegisterSchema } from "@/interface";
-import { HTMLInputTypeAttribute, useCallback } from "react";
-import { useNavigate } from "react-router-dom";
-import styled from "styled-components";
-import { FieldSet } from "./styled/FieldSet";
-import { Label } from "./styled/Label";
+import useRegisterForm from "@/hooks/auth/useRegisterForm"
+import { IRegisterSchema } from "@/interface"
+import { HTMLInputTypeAttribute, useCallback } from "react"
+import { useNavigate } from "react-router-dom"
+import styled from "styled-components"
+import { FieldSet } from "./styled/FieldSet"
+import { Input } from "./styled/Input"
+import { Label } from "./styled/Label"
 
 export const StyledRegisterForm = styled.form`
-	margin: 5rem auto;
-	width: max-content;
-`;
+    margin: 5rem auto;
+    width: max-content;
+`
 
 export default function RegisterForm(props: { children?: any }) {
-	const nav = useNavigate();
+    const nav = useNavigate()
 
-	const {
-		form: {
-			register,
-			onSubmit,
-			formState: { errors },
-		},
-		loading,
-		error,
-	} = useRegisterForm(() => nav("/"));
+    const {
+        form: {
+            register,
+            onSubmit,
+            formState: { errors },
+        },
+        loading,
+        error,
+    } = useRegisterForm(() => nav("/"))
 
-	const Input = useCallback(
-		function Input({
-			type,
-			registerKey,
-		}: {
-			registerKey: keyof IRegisterSchema;
-			type: HTMLInputTypeAttribute;
-		}) {
-			return (
-				<>
-					<Input type={type} {...register(registerKey)} />
-					<p>{errors[registerKey]?.message}</p>
-				</>
-			);
-		},
-		[errors, register]
-	);
+    const RenderInput = useCallback(
+        function ({
+            type,
+            registerKey,
+        }: {
+            registerKey: keyof IRegisterSchema
+            type: HTMLInputTypeAttribute
+        }) {
+            return (
+                <>
+                    <Input type={type} {...register(registerKey)} />
+                    <p>{errors[registerKey]?.message}</p>
+                </>
+            )
+        },
+        [errors, register]
+    )
 
-	return (
-		<StyledRegisterForm onSubmit={onSubmit}>
-			<FieldSet>
-				<Label>Email</Label>
-				<Input registerKey={"email"} type="email" />
-			</FieldSet>
-			<FieldSet>
-				<Label>Name</Label>
-				<Input registerKey={"name"} type="text" />
-			</FieldSet>
-			<FieldSet>
-				<Label>Password</Label>
-				<Input registerKey={"password"} type="password" />
-			</FieldSet>
-			<p>{error.value}</p>
-			{props.children}
-		</StyledRegisterForm>
-	);
+    return (
+        <StyledRegisterForm onSubmit={onSubmit}>
+            <FieldSet>
+                <Label>Email</Label>
+                <RenderInput registerKey={"email"} type="email" />
+            </FieldSet>
+            <FieldSet>
+                <Label>Name</Label>
+                <RenderInput registerKey={"name"} type="text" />
+            </FieldSet>
+            <FieldSet>
+                <Label>Password</Label>
+                <RenderInput registerKey={"password"} type="password" />
+            </FieldSet>
+            <p>{error.value}</p>
+            {props.children}
+        </StyledRegisterForm>
+    )
 }
