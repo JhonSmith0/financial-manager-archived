a77737d7ac0c09105c34ee9e794032a80887aebc
Jhonsmith0 <zamuplays@gmail.com>
2023-06-18 15:01:54 -0300

    feat: account exists transaction controller

--- packages/api/src/controllers/TransactionController.ts
+++ packages/api/src/controllers/TransactionController.ts
@@ -1,4 +1,5 @@
 import { LeftRightHandler } from "@financial/core/dist/common/decorators/LeftRightHandler";
+import { AccountUseCases } from "@financial/core/dist/domain/Account/useCases/AccountUseCases";
 import { CreateTransactionDTO } from "@financial/core/dist/domain/Transaction/dto/CreateTransactionDTO";
 import { DeleteTransactionDTO } from "@financial/core/dist/domain/Transaction/dto/DeleteTransactionDTO";
 import { ReadTransactionDTO } from "@financial/core/dist/domain/Transaction/dto/ReadTransactionDTO";
@@ -7,79 +8,76 @@ import { UpdateTransactionDTO } from "@financial/core/dist/domain/Transaction/dt
 import { TransactionUseCasesFactory } from "@financial/core/dist/domain/Transaction/factory/TransactionUseCasesFactory";
 import User from "@financial/core/dist/domain/User/entity/User";
 import {
-    Body,
-    Controller,
-    Delete,
-    Get,
-    Param,
-    Patch,
-    Post,
+	Body,
+	Controller,
+	Delete,
+	Get,
+	Param,
+	Patch,
+	Post,
+	UseGuards,
 } from "@nestjs/common";
 import { AdaptErrors } from "../adapters/adaptErrors";
 import { UserEntity } from "../decorators/UserEntity";
 
+import { AccountExists } from "../middlewares/AccountExists";
+
 @Controller("transaction")
 export class TransactionController {
-    constructor(private transactionUseCases: TransactionUseCasesFactory) {}
+	constructor(
+		private transactionUseCases: TransactionUseCasesFactory,
+		private accountUseCases: AccountUseCases
+	) {}
 
-    @Post()
-    @AdaptErrors()
-    @LeftRightHandler()
-    public async create(@UserEntity() user: User, @Body() body: any) {
-        const dto = new CreateTransactionDTO(body);
-        const validation = await dto.validate();
-        if (validation.length) throw validation;
+	@UseGuards(AccountExists)
+	@Post()
+	@AdaptErrors()
+	@LeftRightHandler()
+	public async create(
+		@UserEntity() user: User,
+		@Body() dto: CreateTransactionDTO
+	) {
+		return await this.transactionUseCases.create.execute({ dto, user });
+	}
 
-        return await this.transactionUseCases.create.execute({ dto, user });
-    }
+	@Get("search")
+	@AdaptErrors()
+	@LeftRightHandler()
+	public async search(
+		@UserEntity() user: User,
+		@Body() dto: SearchTransactionDTO
+	) {
+		return await this.transactionUseCases.search.execute({ dto, user });
+	}
 
-    @Get("search")
-    @AdaptErrors()
-    @LeftRightHandler()
-    public async search(@UserEntity() user: User, @Body() body: any) {
-        const dto = new SearchTransactionDTO(body);
-        const validation = await dto.validate();
-        if (validation.length) throw validation;
-        return await this.transactionUseCases.search.execute({ dto, user });
-    }
+	@Get("/:id")
+	@AdaptErrors()
+	@LeftRightHandler()
+	public async readTransaction(@Param() dto: ReadTransactionDTO) {
+		return await this.transactionUseCases.read.execute(dto.id);
+	}
 
-    @Get("/:id")
-    @AdaptErrors()
-    @LeftRightHandler()
-    public async readTransaction(@Param() params: any) {
-        const dto = new ReadTransactionDTO(params);
-        const validation = await dto.validate();
-        if (validation.length) throw validation;
-        return await this.transactionUseCases.read.execute(dto.id);
-    }
+	@Delete("/:id")
+	@AdaptErrors()
+	@LeftRightHandler()
+	public async removeTransaction(@Param() dto: DeleteTransactionDTO) {
+		return await this.transactionUseCases.remove.execute({
+			dto: { id: dto.id },
+		});
+	}
 
-    @Delete("/:id")
-    @AdaptErrors()
-    @LeftRightHandler()
-    public async removeTransaction(@Param() params: any) {
-        const dto = new DeleteTransactionDTO(params);
-        const validation = await dto.validate();
-        if (validation.length) throw validation;
-        return await this.transactionUseCases.remove.execute({
-            dto: { id: dto.id },
-        });
-    }
-
-    @Patch("/:id")
-    @AdaptErrors()
-    @LeftRightHandler()
-    public async updateTransaction(
-        @Param() params: any,
-        @Body() body: UpdateTransactionDTO
-    ) {
-        const dto = new UpdateTransactionDTO(body);
-        const validation = await dto.validate();
-        if (validation.length) throw validation;
-        return await this.transactionUseCases.update.execute({
-            dto,
-            transaction: {
-                id: params.id,
-            },
-        });
-    }
+	@Patch("/:id")
+	@AdaptErrors()
+	@LeftRightHandler()
+	public async updateTransaction(
+		@Param() params: any,
+		@Body() dto: UpdateTransactionDTO
+	) {
+		return await this.transactionUseCases.update.execute({
+			dto,
+			transaction: {
+				id: params.id,
+			},
+		});
+	}
 }
--- packages/api/src/server.ts
+++ packages/api/src/server.ts
@@ -1,18 +1,21 @@
+import { ValidationPipe } from "@nestjs/common";
 import { createApp } from "..";
 import { loadEnv } from "@financial/core/dist/utils/loadEnv";
 
 async function main() {
-  // Loads envs from core package
-  loadEnv(process.env.NODE_ENV || "dev");
-  const app = await createApp();
+	// Loads envs from core package
+	loadEnv(process.env.NODE_ENV || "dev");
+	const app = await createApp();
 
-  app.enableCors({
-    origin: "http://localhost:5173",
-    methods: ["GET", "POST", "PATCH", "PUT", "DELETE"],
-    credentials: true,
-  });
+	app.useGlobalPipes(new ValidationPipe());
 
-  app.listen(3000);
+	app.enableCors({
+		origin: "http://localhost:5173",
+		methods: ["GET", "POST", "PATCH", "PUT", "DELETE"],
+		credentials: true,
+	});
+
+	app.listen(3000);
 }
 
 main();
