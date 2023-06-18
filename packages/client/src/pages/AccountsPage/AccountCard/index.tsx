382c1d20f7d49d60e2fc3418fc720209b674fb41
Jhonsmith0 <zamuplays@gmail.com>
2023-06-18 17:20:07 -0300

    feat(client): balance implementation

--- packages/client/src/pages/AccountsPage/AccountCard/index.tsx
+++ packages/client/src/pages/AccountsPage/AccountCard/index.tsx
@@ -1,104 +1,109 @@
-import { IAccount, ITransactionWithAccounts } from "@/interface";
-import { remove, stateUpdateAccount } from "@/state/accountsState";
-import styled from "styled-components";
-import { Bar } from "@/components/styled/Bar";
-import { IconList } from "@/components/styled/IconList";
-import { HiOutlineTrash, HiOutlineCog } from "react-icons/hi";
-import { useHookstate } from "@hookstate/core";
-import { UpdateAccountCard } from "../UpdateAccountCard";
 import {
-	readAccountTransactionsService,
-	updateAccount,
-} from "@/services/account";
-import { useEffect } from "react";
-import {
-	StyledTransactionList,
-	TransactionList,
-} from "@/components/TransactionList";
+    StyledTransactionList,
+    TransactionList,
+} from "@/components/TransactionList"
+import { Balance } from "@/components/styled/Balance"
+import { Bar } from "@/components/styled/Bar"
+import { IconList } from "@/components/styled/IconList"
+import { useAccount } from "@/hooks/accounts/useAccount"
+import { IAccount, ITransactionWithAccounts } from "@/interface"
+import { updateAccount } from "@/services/account"
+import { remove, stateUpdateAccount } from "@/state/accountsState"
+import { useHookstate } from "@hookstate/core"
+import { HiOutlineCog, HiOutlineTrash } from "react-icons/hi"
+import styled from "styled-components"
+import { UpdateAccountCard } from "../UpdateAccountCard"
 
 interface Props {
-	data: IAccount;
+    account: IAccount
 }
 
 export const StyledAccountCard = styled.div`
-	box-shadow: 0px 12px 12px rgb(0, 0, 0, 0.05);
-	border-radius: 4px;
-	overflow: hidden;
+    box-shadow: 0px 12px 12px rgb(0, 0, 0, 0.05);
+    border-radius: 4px;
+    overflow: hidden;
 
-	--padding-inline: 1.4rem;
+    --padding-inline: 1.4rem;
 
-	${Bar} {
-		font-size: 2.4rem;
-		color: white;
-		background: #364fc7;
+    ${Bar} {
+        font-size: 2.4rem;
+        color: white;
+        background: #364fc7;
 
-		justify-content: space-between;
+        justify-content: space-between;
 
-		padding: 0 var(--padding-inline) !important;
+        padding: 0 var(--padding-inline) !important;
 
-		.icons {
-			display: flex;
-			gap: 0.6rem;
-		}
-	}
+        text-transform: capitalize;
 
-	svg {
-		color: #fff;
-	}
+        ${Balance} {
+            margin-right: 1.8rem;
+        }
 
-	${StyledTransactionList} {
-		svg {
-			color: black;
-		}
-	}
-`;
+        .icons {
+            margin-left: auto;
+            display: flex;
+            gap: 0.6rem;
+        }
+    }
 
-export function AccountCard({ data }: Props) {
-	const editing = useHookstate(false);
-	const transactions = useHookstate<ITransactionWithAccounts[]>([]);
+    svg {
+        color: #fff;
+    }
 
-	function onClose() {
-		editing.set(false);
-	}
+    ${StyledTransactionList} {
+        svg {
+            color: black;
+        }
+    }
+`
 
-	async function onSave(data: IAccount) {
-		const newAcc = await updateAccount(data.id, data);
-		onClose();
-		stateUpdateAccount(data.id, newAcc);
-	}
+export function AccountCard({ account: data }: Props) {
+    const editing = useHookstate(false)
 
-	useEffect(() => {
-		void (async function () {
-			readAccountTransactionsService(data.id).then((e) => transactions.set(e));
-		})();
-	}, [data]);
+    const { account, balance, transactions } = useAccount(data.id)
 
-	return (
-		<StyledAccountCard>
-			{editing.get() && (
-				<UpdateAccountCard data={data} onClose={onClose} onSave={onSave} />
-			)}
-			<Bar as={"header"}>
-				<span>{data.name}</span>
-				<IconList>
-					<button onClick={() => editing.set(true)}>
-						<HiOutlineCog />
-					</button>
-					<button
-						onClick={async () => {
-							const value = confirm(
-								`Are you sure you want to delete ${data.name}?`
-							);
-							value && (await remove(data.id));
-						}}
-					>
-						<HiOutlineTrash />
-					</button>
-				</IconList>
-			</Bar>
-			<TransactionList
-				data={transactions.get() as ITransactionWithAccounts[]}
-			/>
-		</StyledAccountCard>
-	);
+    function onClose() {
+        editing.set(false)
+    }
+
+    async function onSave(data: IAccount) {
+        const newAcc = await updateAccount(data.id, data)
+        onClose()
+        stateUpdateAccount(data.id, newAcc)
+    }
+
+    return (
+        <StyledAccountCard>
+            {editing.get() && (
+                <UpdateAccountCard
+                    data={data}
+                    onClose={onClose}
+                    onSave={onSave}
+                />
+            )}
+            <Bar as={"header"}>
+                <span>{data.name}</span>
+                <IconList>
+                    <Balance amount={balance}>R$ {balance}</Balance>
+                    <button onClick={() => editing.set(true)}>
+                        <HiOutlineCog />
+                    </button>
+                    <button
+                        onClick={async () => {
+                            const value = confirm(
+                                `Are you sure you want to delete ${data.name}?`
+                            )
+                            value && (await remove(data.id))
+                        }}
+                    >
+                        <HiOutlineTrash />
+                    </button>
+                </IconList>
+            </Bar>
+            <TransactionList
+                data={transactions as ITransactionWithAccounts[]}
+            />
+        </StyledAccountCard>
+    )
 }
--- packages/client/src/pages/AccountsPage/AccountsList/index.tsx
+++ packages/client/src/pages/AccountsPage/AccountsList/index.tsx
@@ -1,44 +1,44 @@
-import { IAccount } from "@/interface";
-import accountsState from "@/state/accountsState";
-import { useHookstate } from "@hookstate/core";
-import { SearchAccount } from "../SearchAccount";
-import styled from "styled-components";
-import { AccountCard } from "../AccountCard";
-import { Container } from "@/components/styled/Container";
-import { Title } from "@/components/styled/Title";
-import { Button } from "@/components/styled/Button";
+import { Button } from "@/components/styled/Button"
+import { Container } from "@/components/styled/Container"
+import { Title } from "@/components/styled/Title"
+import { IAccount } from "@/interface"
+import accountsState from "@/state/accountsState"
+import { useHookstate } from "@hookstate/core"
+import styled from "styled-components"
+import { AccountCard } from "../AccountCard"
+import { SearchAccount } from "../SearchAccount"
 
 export const StyledAccountList = styled(Container)`
-	margin-top: 0;
-	.list {
-		display: grid;
-		grid-template-columns: repeat(1, 1fr);
-		gap: 4.8rem;
-		margin-top: 4.8rem;
-	}
+    margin-top: 0;
+    .list {
+        display: grid;
+        grid-template-columns: repeat(1, 1fr);
+        gap: 4.8rem;
+        margin-top: 4.8rem;
+    }
 
-	${Button} {
-		padding-block: 0.6rem;
-		align-self: end;
-	}
+    ${Button} {
+        padding-block: 0.6rem;
+        align-self: end;
+    }
 
-	color: #343a40;
-	font-family: "Inter";
-`;
+    color: #343a40;
+    font-family: "Inter";
+`
 
 export function AccountList() {
-	const state = useHookstate<IAccount[]>(accountsState);
-	const accounts = state.get();
+    const state = useHookstate<IAccount[]>(accountsState)
+    const accounts = state.get()
 
-	return (
-		<StyledAccountList>
-			<Title size="medium">Accounts</Title>
-			<SearchAccount />
-			<div className="list">
-				{accounts.map((data) => (
-					<AccountCard data={data} key={data.id} />
-				))}
-			</div>
-		</StyledAccountList>
-	);
+    return (
+        <StyledAccountList>
+            <Title size="medium">Accounts</Title>
+            <SearchAccount />
+            <div className="list">
+                {accounts.map((data) => (
+                    <AccountCard account={data} key={data.id} />
+                ))}
+            </div>
+        </StyledAccountList>
+    )
 }
