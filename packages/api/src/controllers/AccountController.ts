937f7a978f70fc7e36136e54a6e927b75cfd63f6
Jhonsmith0 <zamuplays@gmail.com>
2023-06-18 17:18:59 -0300

    feat(api): implemented account balance end point

--- packages/api/src/controllers/AccountController.ts
+++ packages/api/src/controllers/AccountController.ts
@@ -1,100 +1,109 @@
-import { LeftRightHandler } from "@financial/core/dist/common/decorators/LeftRightHandler";
-import CreateAccountDTO from "@financial/core/dist/domain/Account/dto/CreateAccountDTO";
-import { DeleteAccountDTO } from "@financial/core/dist/domain/Account/dto/DeleteAccountDTO";
-import { ReadAccountDTO } from "@financial/core/dist/domain/Account/dto/ReadAccountDTO";
-import { SearchAccountDTO } from "@financial/core/dist/domain/Account/dto/SearchAccountDTO";
-import UpdateAccountDTO from "@financial/core/dist/domain/Account/dto/UpdateAccountDTO";
-import { AccountUseCases } from "@financial/core/dist/domain/Account/useCases/AccountUseCases";
-import { TransactionUseCasesFactory } from "@financial/core/dist/domain/Transaction/factory/TransactionUseCasesFactory";
-import User from "@financial/core/dist/domain/User/entity/User";
+import { LeftRightHandler } from "@financial/core/dist/common/decorators/LeftRightHandler"
+import CreateAccountDTO from "@financial/core/dist/domain/Account/dto/CreateAccountDTO"
+import { DeleteAccountDTO } from "@financial/core/dist/domain/Account/dto/DeleteAccountDTO"
+import { ReadAccountDTO } from "@financial/core/dist/domain/Account/dto/ReadAccountDTO"
+import { SearchAccountDTO } from "@financial/core/dist/domain/Account/dto/SearchAccountDTO"
+import UpdateAccountDTO from "@financial/core/dist/domain/Account/dto/UpdateAccountDTO"
+import { AccountUseCases } from "@financial/core/dist/domain/Account/useCases/AccountUseCases"
+import { TransactionUseCasesFactory } from "@financial/core/dist/domain/Transaction/factory/TransactionUseCasesFactory"
+import User from "@financial/core/dist/domain/User/entity/User"
 import {
-	BadRequestException,
-	Body,
-	Controller,
-	Delete,
-	Get,
-	Param,
-	Patch,
-	Post,
-	Query,
-} from "@nestjs/common";
-import { AdaptErrors } from "../adapters/adaptErrors";
-import { UserEntity } from "../decorators/UserEntity";
+    BadRequestException,
+    Body,
+    Controller,
+    Delete,
+    Get,
+    Param,
+    Patch,
+    Post,
+    Query,
+} from "@nestjs/common"
+import { AdaptErrors } from "../adapters/adaptErrors"
+import { UserEntity } from "../decorators/UserEntity"
 
 @Controller("account")
 export class AccountController {
-	constructor(
-		private accountUseCases: AccountUseCases,
-		private transactionUseCases: TransactionUseCasesFactory
-	) {}
+    constructor(
+        private accountUseCases: AccountUseCases,
+        private transactionUseCases: TransactionUseCasesFactory
+    ) {}
 
-	@Post()
-	@AdaptErrors()
-	@LeftRightHandler()
-	async createAccount(
-		@UserEntity() user: User,
-		@Body() body: ClassProperties<CreateAccountDTO>
-	) {
-		return await this.accountUseCases.create.execute({
-			...body,
-			userId: user.id,
-		});
-	}
+    @Post()
+    @AdaptErrors()
+    @LeftRightHandler()
+    async createAccount(
+        @UserEntity() user: User,
+        @Body() body: ClassProperties<CreateAccountDTO>
+    ) {
+        return await this.accountUseCases.create.execute({
+            ...body,
+            userId: user.id,
+        })
+    }
 
-	@Get("search")
-	@AdaptErrors()
-	@LeftRightHandler()
-	async searchAccount(
-		@UserEntity()
-			user: User,
-		@Query() dto: SearchAccountDTO
-	) {
-		return await this.accountUseCases.search.execute({
-			dto,
-			user: { id: user.id },
-		});
-	}
+    @Get("search")
+    @AdaptErrors()
+    @LeftRightHandler()
+    async searchAccount(
+        @UserEntity()
+        user: User,
+        @Query() dto: SearchAccountDTO
+    ) {
+        return await this.accountUseCases.search.execute({
+            dto,
+            user: { id: user.id },
+        })
+    }
 
-	@Patch("/:id")
-	@AdaptErrors()
-	@LeftRightHandler()
-	async updateAccount(
-		@UserEntity() user: User,
-		@Body() body: any,
-		@Param() dto: UpdateAccountDTO
-	) {
-		return await this.accountUseCases.update.execute({ user, dto });
-	}
+    @Patch("/:id")
+    @AdaptErrors()
+    @LeftRightHandler()
+    async updateAccount(
+        @UserEntity() user: User,
+        @Body() body: any,
+        @Param() dto: UpdateAccountDTO
+    ) {
+        return await this.accountUseCases.update.execute({ user, dto })
+    }
 
-	@Delete("/:id")
-	@AdaptErrors()
-	@LeftRightHandler()
-	async deleteAccount(
-		@UserEntity() user: User,
-		@Param() dto: DeleteAccountDTO
-	) {
-		await this.transactionUseCases.removeAll.execute(dto.id)
+    @Delete("/:id")
+    @AdaptErrors()
+    @LeftRightHandler()
+    async deleteAccount(
+        @UserEntity() user: User,
+        @Param() dto: DeleteAccountDTO
+    ) {
+        await this.transactionUseCases.removeAll.execute(dto.id)
 
-		return await this.accountUseCases.remove.execute({ dto: dto, user });
-	}
+        return await this.accountUseCases.remove.execute({ dto: dto, user })
+    }
 
-	@Get("/:id")
-	@AdaptErrors()
-	@LeftRightHandler()
-	async getAccount(@UserEntity() user: User, @Param() dto: ReadAccountDTO) {
-		return await this.accountUseCases.read.execute({ accountId: dto.id });
-	}
-	@Get("/:accountId/transactions")
-	@AdaptErrors()
-	@LeftRightHandler()
-	async getAccountTransactions(
-		@UserEntity() user: User,
-		@Param() params: { accountId: string }
-	) {
-		if (!params.accountId || typeof params.accountId !== "string")
-			throw new BadRequestException("Invalid data!");
-		return await this.transactionUseCases.accountTransactions.execute({
-			accountId: params.accountId,
-		});
-	}
+    @Get("/:id")
+    @AdaptErrors()
+    @LeftRightHandler()
+    async getAccount(@UserEntity() user: User, @Param() dto: ReadAccountDTO) {
+        return await this.accountUseCases.read.execute({ accountId: dto.id })
+    }
+    @Get("/:accountId/transactions")
+    @AdaptErrors()
+    @LeftRightHandler()
+    async getAccountTransactions(
+        @UserEntity() user: User,
+        @Param() params: { accountId: string }
+    ) {
+        if (!params.accountId || typeof params.accountId !== "string")
+            throw new BadRequestException("Invalid data!")
+        return await this.transactionUseCases.accountTransactions.execute({
+            accountId: params.accountId,
+        })
+    }
+    @Get("/:accountId/balance")
+    @AdaptErrors()
+    @LeftRightHandler()
+    async accountBalance(
+        @UserEntity() user: User,
+        @Param() params: { accountId: string }
+    ) {
+        return await this.accountUseCases.balance.execute(params.accountId)
+    }
 }
--- packages/core/src/domain/Account/useCases/AccountUseCases.ts
+++ packages/core/src/domain/Account/useCases/AccountUseCases.ts
@@ -1,16 +1,18 @@
-import { AccountRepository } from "../repo/AccountRepository";
-import CreateAccountUseCase from "./CreateAccountUseCase";
-import { DeleteAccountUseCase } from "./DeleteAccountUseCase";
-import { ReadAccountUseCase } from "./ReadAccountUseCase.1";
-import { SearchAccountUseCase } from "./SearchAccountUseCase";
-import { UpdateAccountUseCase } from "./UpdateAccountUseCase";
+import { AccountRepository } from "../repo/AccountRepository"
+import { AccountBalanceUseCase } from "./AccountBalanceUseCase"
+import CreateAccountUseCase from "./CreateAccountUseCase"
+import { DeleteAccountUseCase } from "./DeleteAccountUseCase"
+import { ReadAccountUseCase } from "./ReadAccountUseCase.1"
+import { SearchAccountUseCase } from "./SearchAccountUseCase"
+import { UpdateAccountUseCase } from "./UpdateAccountUseCase"
 
 export class AccountUseCases {
-	constructor(private repo: AccountRepository) {}
+    constructor(private repo: AccountRepository) {}
 
-	public create = new CreateAccountUseCase(this.repo);
-	public read = new ReadAccountUseCase(this.repo);
-	public update = new UpdateAccountUseCase(this.repo);
-	public remove = new DeleteAccountUseCase(this.repo);
-	public search = new SearchAccountUseCase(this.repo);
+    public create = new CreateAccountUseCase(this.repo)
+    public read = new ReadAccountUseCase(this.repo)
+    public update = new UpdateAccountUseCase(this.repo)
+    public remove = new DeleteAccountUseCase(this.repo)
+    public search = new SearchAccountUseCase(this.repo)
+    public balance = new AccountBalanceUseCase(this.repo)
 }
