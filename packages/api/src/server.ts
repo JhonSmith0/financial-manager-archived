94154e971d054d31e6d05b8a4eaacc61e1dc2778
Jhonsmith0 <zamuplays@gmail.com>
2023-06-18 16:52:15 -0300

    refactor: remove page from search account dto

--- packages/api/src/server.ts
+++ packages/api/src/server.ts
@@ -1,21 +1,28 @@
-import { ValidationPipe } from "@nestjs/common";
-import { createApp } from "..";
-import { loadEnv } from "@financial/core/dist/utils/loadEnv";
+import { loadEnv } from "@financial/core/dist/utils/loadEnv"
+import { ValidationPipe } from "@nestjs/common"
+import { createApp } from ".."
 
 async function main() {
-	// Loads envs from core package
-	loadEnv(process.env.NODE_ENV || "dev");
-	const app = await createApp();
+    // Loads envs from core package
+    loadEnv(process.env.NODE_ENV || "dev")
+    const app = await createApp()
 
-	app.useGlobalPipes(new ValidationPipe());
+    app.useGlobalPipes(
+        new ValidationPipe({
+            transformOptions: {
+                exposeDefaultValues: true,
+                excludeExtraneousValues: true,
+            },
+        })
+    )
 
-	app.enableCors({
-		origin: "http://localhost:5173",
-		methods: ["GET", "POST", "PATCH", "PUT", "DELETE"],
-		credentials: true,
-	});
+    app.enableCors({
+        origin: "http://localhost:5173",
+        methods: ["GET", "POST", "PATCH", "PUT", "DELETE"],
+        credentials: true,
+    })
 
-	app.listen(3000);
+    app.listen(3000)
 }
 
-main();
+main()
--- packages/core/src/domain/Account/dto/SearchAccountDTO.ts
+++ packages/core/src/domain/Account/dto/SearchAccountDTO.ts
@@ -1,35 +1,25 @@
-import DTO from "@/common/DTO/DTO";
-import { Transformer } from "@/common/Transformer";
-import { Expose } from "class-transformer";
-import { IsNumber, IsOptional, IsString, Min } from "class-validator";
+import DTO from "@/common/DTO/DTO"
+import { Transformer } from "@/common/Transformer"
+import { Expose } from "class-transformer"
+import { IsOptional, IsString } from "class-validator"
 
 export class SearchAccountDTO extends DTO {
-  @Expose()
-  @IsString()
-  public name: string;
+    @Expose()
+    @IsString()
+    @IsOptional()
+    public name: string = ""
 
-  @Expose()
-  @IsString()
-  @IsOptional()
-  public description: string = "";
+    @Expose()
+    @IsString()
+    @IsOptional()
+    public description: string = ""
 
-  @IsNumber()
-  @Expose()
-  @Min(1)
-  @IsOptional()
-  public page: number = 1;
-
-  public static create(data: SearchAccountDTOProps) {
-    data.page &&= +data.page;
-    return Transformer.plainToInstance(SearchAccountDTO, data, {
-      exposeDefaultValues: true,
-    });
-  }
+    public static create(data: SearchAccountDTOProps) {
+        return Transformer.plainToInstance(SearchAccountDTO, data, {
+            exposeDefaultValues: true,
+        })
+    }
 }
 
 export interface SearchAccountDTOProps
-  extends OptionalProps<
-    ClassProperties<SearchAccountDTO>,
-    "page" | "description"
-  > {}
-
+    extends OptionalProps<ClassProperties<SearchAccountDTO>, "description"> {}
