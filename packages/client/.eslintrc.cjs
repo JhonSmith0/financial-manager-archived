29e6e742ed0add9ec90e27b150f41aa2ef509f36
Jhonsmith0 <zamuplays@gmail.com>
2023-06-18 16:10:58 -0300

    refactor: using prettier

--- packages/client/.eslintrc.cjs
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	root: true,
-	extends: ["./node_modules/eslint-custom"],
-	rules: {
-		"@typescript-eslint/no-explicit-any": "off",
-		"@typescript-eslint/no-unused-vars": "off",
-		"@typescript-eslint/no-empty-function": "off",
-	},
-};
