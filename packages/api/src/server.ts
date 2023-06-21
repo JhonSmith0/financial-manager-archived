c616074d14f99aa2bc2cdb1362068dd5d8f338de
Jhonsmith0 <zamuplays@gmail.com>
2023-06-20 21:29:44 -0300

    fix: fixed bug nestjs doesnot transform dto

--- packages/api/src/server.ts
+++ packages/api/src/server.ts
@@ -8,18 +8,18 @@ async function main() {
     // Loads envs from core package
     loadEnv(process.env.NODE_ENV || "dev")
     const app = await createApp()
-
-    app.useGlobalInterceptors(new AdaptLeftRightInterceptor())
-    app.useGlobalFilters(new AllExceptionFilter())
-
     app.useGlobalPipes(
         new ValidationPipe({
+            transform: true,
             transformOptions: {
                 exposeDefaultValues: true,
                 excludeExtraneousValues: true,
             },
         })
     )
+
+    app.useGlobalInterceptors(new AdaptLeftRightInterceptor())
+    app.useGlobalFilters(new AllExceptionFilter())
 
     app.enableCors({
         origin: "http://localhost:5173",
