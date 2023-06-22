da7bc4e97938504edae2bb5eae8a7d8b9fcdf98e
Jhonsmith0 <zamuplays@gmail.com>
2023-06-21 21:01:06 -0300

    test: added init prop to create test from module

--- packages/api/test/utils/createTestFromModule.ts
+++ packages/api/test/utils/createTestFromModule.ts
@@ -3,13 +3,15 @@ import { ModuleMetadata } from "@nestjs/common"
 import { Test } from "@nestjs/testing/test"
 import supertest from "supertest"
 
-export async function createTestFromModule(data: ModuleMetadata) {
+export async function createTestFromModule(data: ModuleMetadata, init = true) {
     const module = await Test.createTestingModule(data).compile()
 
     const app = module.createNestApplication()
     const test = supertest(app.getHttpServer())
 
     configureApp(app)
+
+    init && (await app.init())
 
     return { module, app, test }
 }
