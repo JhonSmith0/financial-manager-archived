459836eec6141637adc78e2cef9614ad4d430a0b
Jhonsmith0 <zamuplays@gmail.com>
2023-06-22 21:12:59 -0300

    feat(api): path mapping

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,8 +1,5 @@
 import { NestFactory } from "@nestjs/core"
 import RootModule from "./modules/RootModule"
-import { registerPaths } from "./registerPaths"
-
-registerPaths()
 
 export async function createApp() {
     return await NestFactory.create(RootModule)
--- packages/api/test/setup/index.ts
+++ packages/api/test/setup/index.ts
@@ -1,6 +1,3 @@
-import { registerPaths } from "../../src/registerPaths"
 import { loadEnv } from "@financial/core/dist/utils/loadEnv"
 
 loadEnv("test")
-
-registerPaths()
