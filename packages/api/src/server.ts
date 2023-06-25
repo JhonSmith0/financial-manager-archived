f71154262de2c82f0453e8991e9711699a75673b
Jhonsmith0 <zamuplays@gmail.com>
2023-06-25 18:34:04 -0300

    refactor: loading env from api folder

--- packages/api/src/server.ts
+++ packages/api/src/server.ts
@@ -1,10 +1,16 @@
-import { loadEnv } from "@financial/core/dist/utils/loadEnv"
+import { config } from "dotenv"
+import { resolve } from "path"
 import { createApp } from "."
 import { configureApp } from "./configureApp"
 
+config({
+    path: resolve(__dirname, "../.env"),
+    override: false,
+})
+
 async function main() {
     // Loads envs from core package
-    loadEnv(process.env.NODE_ENV || "dev")
+    console.log(process.env)
     const app = await createApp()
     configureApp(app)
 
